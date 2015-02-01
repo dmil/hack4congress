09919ff5d151d754a866eab5b5f3603c98005b68
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2015-01-31 19:48:21 -0500

    update csvtojson

--- data/csvtojson.py
+++ data/csvtojson.py
@@ -7,8 +7,8 @@ jsonfile = open('file.json', 'w')
 fieldnames = ("Time", "Name", "Type of Organization", "Comment")
 reader = csv.DictReader( csvfile, fieldnames)
 for row in reader:
-	for i in range(1, 7):
-		if i != 3 and i != 5 and i != 7:
-			final_row = final_row + "," + row[i] + "\n"
+  for i in range(1, 7):
+    if i != 3 and i != 5 and i != 7:
+      final_row = final_row + "," + row[i] + "\n"
     json.dump(final_row, jsonfile)
     jsonfile.write('\n')
