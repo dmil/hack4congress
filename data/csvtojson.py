b3ce768890efc0b0c50e3568ba776d6d1d8266ba
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2015-01-31 21:20:45 -0500

    improve csvtojson data converter

--- data/csvtojson.py
+++ data/csvtojson.py
@@ -1,16 +1,15 @@
 import csv
 import json
 
-csvfile = open('1000-comments.csv', 'r')
 
-fieldnames = ("Time", "Name", "Type of Organization", "Comment")
-reader = csv.DictReader(csvfile)
+with open('data/1000-comments-correct.csv', 'r') as csvfile: 
+  reader = csv.DictReader(csvfile)
 
-rows = []
-for row in reader:
-  row = dict((key,value) for key, value in row.iteritems() if key == "Name" or key == "Comment")
-  rows.append(row)
+  rows = []
+  for row in reader:
+    row = dict((key,value) for key, value in row.iteritems() if key == "Name" or key == "Comment")
+    rows.append(row)
 
 
-with open('file.json', 'w') as jsonfile:
+with open('data/comment.json', 'w') as jsonfile:
   json.dump(rows, jsonfile, indent=2)
